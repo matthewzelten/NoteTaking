f611e5d759c71aeb3a7b374a47168195265b33f3
Vincent Pheng <vincentpheng@gmail.com>
2021-10-25 10:56:54 -0700

    Rework create pages into react-modals

--- frontend/src/components/CreateFolder.js
+++ frontend/src/components/CreateFolder.js
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom"
 
 function CreateFolder(props) {
     const [newFolderName, setNewFolderName] = useState("")
+
+    function submitFolderName() {
+        props.setFolderName(newFolderName)
+        props.setShowModal(false)
+    }
+
     return (
         <div>
             <h1>Add New Folder</h1>
             <input type="text" placeholder="Enter Folder Name" onChange={(e) => setNewFolderName(e.target.value)}/>
             <Link to="/folder">
-                <button onClick={() => props.setFolderName(newFolderName)}>Create</button>
+                <button onClick={() => submitFolderName()}>Create</button>
             </Link>
         </div>
     );
