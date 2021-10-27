9c40e85833c0f50885f4f648936be7de7481cd3b
matthewzelten <57920495+matthewzelten@users.noreply.github.com>
2021-10-27 11:15:56 -0700

    install and run prettier

--- frontend/src/components/shared/FileSettings.js
+++ frontend/src/components/shared/FileSettings.js
@@ -1,59 +1,78 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function FileSettings(props) {
-    const [disablePassword, setDisablePassword] = useState(true)
-    return (
-        <div>
-            <div>
-                <h2>
-                    Choose Color
-                    <p class="create-space" style={{'color':'blue'}}>
-                        blue
-                        <input class="create-space" type="checkbox" />
-                    </p>
-                    <p class="create-space" style={{'color':'green'}}>
-                        green
-                        <input class="create-space" type="checkbox" />
-                    </p>
-                    <p class="create-space" style={{'color':'red'}}>
-                        red
-                        <input class="create-space" type="checkbox" />
-                    </p>
-                </h2>
-            </div>
-            <PrivateNote data={disablePassword}/>
-        </div>
-    )
+  const [disablePassword, setDisablePassword] = useState(true);
+  return (
+    <div>
+      <div>
+        <h2>
+          Choose Color
+          <p class="create-space" style={{ color: "blue" }}>
+            blue
+            <input class="create-space" type="checkbox" />
+          </p>
+          <p class="create-space" style={{ color: "green" }}>
+            green
+            <input class="create-space" type="checkbox" />
+          </p>
+          <p class="create-space" style={{ color: "red" }}>
+            red
+            <input class="create-space" type="checkbox" />
+          </p>
+        </h2>
+      </div>
+      <PrivateNote data={disablePassword} />
+    </div>
+  );
 }
 
 function PrivateNote(props) {
-    const [disablePassword, setDisablePassword] = useState(true)
-    if(disablePassword) {
-        return (
-            <div>
-                <h2>
-                    Set as private?  
-                    <button onClick={() => setDisablePassword(!disablePassword)}>yes</button>
-                    <input class="create-space" type="checkbox" onClick={() => setDisablePassword(!disablePassword)}/>
-                </h2>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div>
-                <div>
-                    <h2>
-                        Set as private?   
-                        <button onClick={() => setDisablePassword(!disablePassword)}>no</button>
-                        <input class="create-space" type="checkbox" onClick={() => setDisablePassword(!disablePassword)}/>
-                    </h2>
-                </div>
-                <input disabled={disablePassword} type="text" placeholder="Enter Password"/>
-                <input disabled={disablePassword} type="text" placeholder="Confirm Password"/>
-            </div>
-        )
-    }
+  const [disablePassword, setDisablePassword] = useState(true);
+  if (disablePassword) {
+    return (
+      <div>
+        <h2>
+          Set as private?
+          <button onClick={() => setDisablePassword(!disablePassword)}>
+            yes
+          </button>
+          <input
+            class="create-space"
+            type="checkbox"
+            onClick={() => setDisablePassword(!disablePassword)}
+          />
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>
+          <h2>
+            Set as private?
+            <button onClick={() => setDisablePassword(!disablePassword)}>
+              no
+            </button>
+            <input
+              class="create-space"
+              type="checkbox"
+              onClick={() => setDisablePassword(!disablePassword)}
+            />
+          </h2>
+        </div>
+        <input
+          disabled={disablePassword}
+          type="text"
+          placeholder="Enter Password"
+        />
+        <input
+          disabled={disablePassword}
+          type="text"
+          placeholder="Confirm Password"
+        />
+      </div>
+    );
+  }
 }
 
 export default FileSettings;
