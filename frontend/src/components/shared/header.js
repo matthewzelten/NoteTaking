760fa260434dc4af3e0efef04b6f8177ed543215
matthewzelten <57920495+matthewzelten@users.noreply.github.com>
2021-10-27 18:31:37 -0700

    create link to / for the header

--- frontend/src/components/shared/header.js
+++ frontend/src/components/shared/header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 function Header(props) {
   return (
     <header class="blue alter-height">
-      <h1>Notes Made Easy</h1>
+      <Link to="/">
+        <h1>Notes Made Easy</h1>
+      </Link>
     </header>
   );
 }
