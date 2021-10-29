01455f079f5f1e12de91ab7534478ca3170b15ae
Vincent Pheng <vincentpheng@Vincents-MacBook-Pro.local>
2021-10-29 10:48:46 -0700

    Possibly removed draft

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import {EditorState} from "draft-js";
 import {Editor} from "react-draft-wysiwyg"
@@ -28,17 +27,3 @@ function Note(props) {
 }
 
 export default Note
-=======
-import React from "react";
-import PropTypes from "prop-types";
-
-function Note(props) {
-  return (
-    <div>
-      <input style={{ width: "100%", height: "200px" }} placeholder={"Notes"} />
-    </div>
-  );
-}
-
-export default Note;
->>>>>>> origin/main
