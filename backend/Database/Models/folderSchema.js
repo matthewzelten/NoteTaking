4cd45e03fd597e5f1b63d986d70b5685032653f6
TheShinySnivy <magicwand452@gmail.com>
2021-10-29 11:18:57 -0700

    added folderSchema base code

--- backend/Database/Models/folderSchema.js
+++ backend/Database/Models/folderSchema.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const noteSchema = require("noteSchema");
+
+const collectionName = "placeholder";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -6,15 +9,20 @@ const UserSchema = new mongoose.Schema({
         required: true,
         trim: true,
     },
-    job: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if (value.length < 2) throw new Error("Invalid job.");
-        },
+    isPrivate: {
+      type: Boolean,
+        required: true
     },
-}, {collection : 'users_list'});
+    password: {
+      type: String,
+        required: false,
+        trim: true
+    },
+    notes: {
+        type: [noteSchema],
+        required: true
+    }
+}, {collection : collectionName});
 
 const User = mongoose.model("User", UserSchema);
 
