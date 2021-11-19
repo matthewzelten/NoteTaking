94091475bfc7bf86c51ba0996209b401fdb37efe
Tong Zhou <tongzhou@kokodayou.local>
2021-11-19 11:29:53 -0800

    modified methods to open and add folder and note. And implement search folder and note

--- backend/sample.js
+++ backend/sample.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 const { json } = require("express");
+const e = require("express");
 
 const folders = {
   folderList: [
@@ -31,18 +32,18 @@ async function getAllFolders() {
 }
 
 async function findFolder(name) {
-  return Folder.find({'name':name});
+  return await Folder.find({'name':name});
+}
+
+async function findNote(folderName, noteName) {
+  return await Note.find({'name':noteName, 'folder':folderName});
 }
 async function findNotesByFolder(fName){
-  return Note.find({'folder':fName});
-}
-async function findNote(folderName, noteName) {
-  return Note.find({'name':noteName, 'folder':folderName});
+  return await Note.find({'folder':fName});
 }
 async function addFolder(folder) {
   Folder.insertOne(folder);
 }
-
 async function deleteFolder(folderToDelete) {
   const folder = Folder.find({'name':folderToDelete});
   if(folder===undefined||folder.length==0){
@@ -81,81 +82,152 @@ async function deleteNote(fName, noteName) {
 app.use(express.json());
 // main page: get all folders
 app.get("/", async (req, res) => {
-  const allFolders = await getAllFolders()
+  const allFolders = await getAllFolders();
   res.send(allFolders);
 });
-// folder page: get all notes
+
+// handle open folder, add folder and search folder
 //I use post methods here because it needs to get password for private folders 
 //and I can't think of an alternative. 
 //Please comment if you have any suggestions or alternative solutions!!!
-app.post("/:folderName", (req, res) => {
+app.post("/", async (req, res)=>{
+  const folder = req.body["folderName"];
+  const keyword = req.body["keyword"];
+  if((folder===undefined||folder.length==0)&&(keyword===undefined||keyword.length==0)){
+    let isDup = await findFolder(req.body["name"]);
+    if (isDup===undefined||isDup.length==0) {
+      const folderToAdd = new Folder(req.body);
+      folderToAdd.save()
+      res.status(200).send(folderToAdd).end();
+    } else {
+      res.status(404).send("Duplicate file name. "+isDup).end();
+    }
+  }
+  else if(keyword===undefined||keyword.length==0){
+    let result = await findFolder(folder);
+    if (result === undefined || result.length == 0) {
+      res.status(404).send("Folder not found.");
+    } else {
+      if (!result["isPrivate"]) {
+        res.status(200).send(result);
+      } else {
+        let passw = req.body["password"];
+        if (passw["password"] === result["password"]) {
+          //res.status(200).send(result);
+          res.status(200).send("get notes");
+          //res.status(200).send(findNotesByFolder(folderName));
+        } else {
+          //res.status(404).send(req.body);
+          res.status(404).send("Wrong password. Access denied.");
+        }
+      }
+    }
+  }
+  else{
+    //search folder
+    Folder.find({"name":{"$regex": keyword, "$options":"i"}}).exec(function (err, docs) {
+      if (err){
+          console.log(err);
+      }else{
+        res.status(200).send(docs).end();
+      }});
+    //res.status(200).send(result).end();
+  }
+})
+function searchFolder(keyword){
+
+}
+/*add folder
+ Comment: These two are merged into one post function (see above). But I keep them for now just in case.
+app.post("/", async (req, res) => {
+  //const {name, color, isPrivate, password} = req.body;
+  let isDup = await findFolder(req.body["name"]);
+  if (isDup===undefined||isDup.length==0) {
+    const folderToAdd = new Folder(req.body);
+    folderToAdd.save()
+    res.status(200).send(folderToAdd).end();
+  } else {
+    res.status(404).send("Duplicate file name. "+isDup).end();
+  }
+});
+app.post("/:folderName", async (req, res) => {
   const folderName = req.params["folderName"];
   const passw = req.body;
-  result = findFolder(folderName);
+  let result = await findFolder(folderName);
   if (result === undefined || result.length == 0) {
     res.status(404).send("Folder not found.");
   } else {
     if (!result["isPrivate"]) {
-      result = findNotesByFolder(folderName);
+      //result = findNotesByFolder(folderName);
+      //res.status(201).send(findNotesByFolder(folderName));
       res.status(201).send(result);
     } else {
       if (passw["password"] === result["password"]) {
-        res.status(201).send(findNotesByFolder(folderName));
+        res.status(201).send("get notes");
+        //res.status(201).send(findNotesByFolder(folderName));
       } else {
         //res.status(404).send(req.body);
         res.status(404).send("Wrong password. Access denied.");
       }
     }
   }
-});
-//open note
+});*/
+
+
+//handle open note and add note
 //I use post methods here because it needs to get password for private notes 
 //and I can't think of an alternative. 
 //Please comment if you have any suggestions or alternative solutions!!!
-app.post("/:folderName/:note", (req, res) => {
+app.post("/:folderName", async (req, res) => {
   const fName = req.params["folderName"];
-  const noteToGet = req.params["note"];
-  result = findFolder(fName);
-  if (result === undefined || result.length == 0) {
-    res.status(404).send("Folder not found.");
-  } else {
-    result = findNote(fName, noteToGet);
+  const note = req.body["noteName"];
+  const keyword = req.body["keyword"];
+  if((note===undefined||note.length==0)&&(keyword===undefined||keyword.length==0)){
+    //add note
+    const noteToAdd = req.body;
+    let result = await findNote(fName, req.body["name"]);
+    if(result===undefined||result.length==0){
+      if(addNote(noteToAdd)){
+        res.status(201).send(noteToAdd).end();
+      }
+      else{
+        res.status(404).end();
+      }
+    }
+    else{
+      res.status(404).send("Duplicate file name.").end();
+    }
+  }
+  else if(keyword===undefined||keyword.length==0){
+    result = await findFolder(fName);
     if (result === undefined || result.length == 0) {
-      res.status(404).send("Note not found.");
+      res.status(404).send("Folder not found.");
     } else {
-      if (!result["isPrivate"]) {
-        res.status(201).send(result).end();
+      result = await findNote(fName, noteToGet);
+      if (result === undefined || result.length == 0) {
+        res.status(404).send("Note not found.");
       } else {
-        const passw = req.body;
-        if (passw["password"] === result["password"]) {
-          res.status(201).send(result).end();
+        if (!result["isPrivate"]) {
+          res.status(200).send(result).end();
         } else {
-          res.status(404).send("Wrong password. Access denied.");
+          const passw = req.body;
+          if (passw["password"] === result["password"]) {
+            res.status(200).send(result).end();
+          } else {
+            res.status(404).send("Wrong password. Access denied.");
+          }
         }
       }
     }
   }
-});
-//search folder
-app.get("/:color", (req, res)=>{
-  const color = req.params['color'];
-  let result = Folder.find({'color':color});
-  res.send(result).end();
-})
-//search note
-app.post("/:folderName", (req, res)=>{
-  const keyword = req.body["keyword"];
-})
-//add folder
-app.post("/", (req, res) => {
-  const {name, color, isPrivate} = req.body;
-  isDup = findFolder(name);
-  if (true) {
-    const folderToAdd = new Folder({name, color, isPrivate});
-    folderToAdd.save()
-    res.status(201).send(folderToAdd).end();
-  } else {
-    //res.status(404).send("Duplicate file name.").end();
+  else{
+    //search note
+    Note.find({"name":{"$regex": keyword, "$options":"i"}}).exec(function (err, docs) {
+      if (err){
+          console.log(err);
+      }else{
+        res.status(200).send(docs).end();
+      }});
   }
 });
 //add note
@@ -172,14 +244,23 @@ async function addNote(note){
     return false;
   }
 }
-app.post("/:folderName", (req, res) => {
-  const note = req.body;
-  if (addNote(req.body)) {
-    res.status(201).end();
-  } else {
-    res.status(404).end();
+/* merged into one post function. but I keep this for now just in case.
+app.post("/:folderName", async (req, res) => {
+  const noteToAdd = req.body;
+  const fName = req.params["folderName"];
+  let result = await findNote(fName, req.body["name"]);
+  if(result===undefined||result.length==0){
+    if(addNote(noteToAdd)){
+      res.status(201).send(noteToAdd).end();
+    }
+    else{
+      res.status(404).end();
+    }
   }
-});
+  else{
+    res.status(404).send("Duplicate file name.").end();
+  }
+});*/
 //delete folder
 app.delete("/", (req, res) => {
   const folderToDelete = req.body["name"];
