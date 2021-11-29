a61286c496ab126b8e6c209ab24bea949e2c155e
matthewzelten <57920495+matthewzelten@users.noreply.github.com>
2021-11-29 11:44:59 -0800

    write tests and modify related stuff

--- backend/sample.test.js
+++ backend/sample.test.js
@@ -1,7 +1,166 @@
 const { test } = require("@jest/globals");
+const mongoose = require("mongoose");
+const FolderSchema = require("./Database/Models/folderSchema");
+const connections = require("./connections");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
-test("test that verify passwords works", () => {
-    const passwordA = 'theSame';
-    const passwordB = 'theSame';
-    expect(true).toBeTruthy();
-})
+let mongoServer;
+let conn;
+let folderModel;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  conn = await mongoose.createConnection(uri, mongooseOpts);
+
+  folderModel = conn.model("Folder", FolderSchema);
+
+  connections.setConnection(conn);
+});
+
+afterAll(async () => {
+  await conn.dropDatabase();
+  await conn.close();
+  await mongoServer.stop();
+});
+
+beforeEach(async () => {
+  let dummyFolder= {
+    name: "public_folder_1",
+    color: "C83E4D",
+    isPrivate: "false"
+  };
+  let result = new folderModel(dummyFolder);
+  await result.save();
+
+  dummyFolder= {
+    name: "private_folder_1",
+    color: "C83E4D",
+    isPrivate: "true",
+    password: "password"
+  };
+  result = new folderModel(dummyFolder);
+  await result.save();
+
+  dummyFolder= {
+    name: "public_folder_2",
+    color: "F4D6CC",
+    isPrivate: "false"
+  };
+  result = new folderModel(dummyFolder);
+  await result.save();
+
+  dummyFolder= {
+    name: "private_folder_2",
+    color: "F4D6CC",
+    isPrivate: "true",
+    password: "hello"
+  };
+  result = new folderModel(dummyFolder);
+  await result.save();
+});
+
+afterEach(async () => {
+  await folderModel.deleteMany();
+});
+
+test("test getAllFolders", async () => {
+    let folders = await connections.getAllFolders();
+    expect(folders.length).toEqual(4);
+    expect(folder[0].name).toEqual('public_folder_1');
+    expect(folder[1].name).toEqual('private_folder_1');
+    expect(folder[2].name).toEqual('public_folder_2');
+    expect(folder[3].name).toEqual('private_folder_2');
+});
+
+test("test findFolder", async () => {
+    let allFolders = await connections.getAllFolders();
+    
+    //find with no parameters
+    let folders = await connections.findFolder({});
+    expect(folders.length).toEqual(4);
+    
+    //find with public folder name
+    let pub1 = folders.findFolder('public_folder_1');
+    expect(pub1.name).toEqual('public_folder_1');
+    expect(pub1.isPrivate).toBeFalsy();
+    expect(pub1.color).toEqual('C83E4D');
+
+    //find with private folder name
+    let priv1 = folders.findFolder('private_folder_1');
+    expect(priv1.name).toEqual('private_folder_1');
+    expect(priv1.isPrivate).toBeTruthy();
+    expect(priv1.color).toEqual('C83E4D');
+    expect(priv1.password).toEqual('password');
+});
+
+test('test findNote', () => {
+    //looks like bad func atm
+});
+
+test('test addFolder', async () => {
+    let allFolders = await connections.getAllFolders();
+    expect(allFolders.length).toEqual(4);
+    
+    // add public folder
+    let newFolder1 = {
+        name: 'testFolder1',
+        color: '0000FF',
+        isPrivate: false
+    }
+    connections.addFolder(new connections.Folder(newFolder1));
+    allFolders = await connections.getAllFolders();
+    expect(allFolders.length).toEqual(5);
+    let finder = await connections.findFolder('testFolder1');
+    expect(finder.color).toEqual('0000FF');
+
+    //add private folder
+    let newFolder2 = {
+        name: 'testFolder2',
+        color: 'FF0000',
+        isPrivate: true,
+        password: 'csc307'
+    }
+    connections.addFolder(new connections.Folder(newFolder2));
+    allFolders = await connections.getAllFolders();
+    expect(allFolders.length).toEqual(6);
+    finder = await connections.findFolder('testFolder2');
+    expect(finder.color).toEqual('FF0000');
+    expect(finder.isPrivate).toBeTruthy();
+    expect(finder.password).toEqual('csc307');
+});
+
+test('test addNote', () => {
+    //this looks like itll be refactored
+});
+
+test('test deleteFolder', async () => {
+    let allFolders = await connections.getAllFolders();
+    expect(allFolders.length).toEqual(4);
+
+    //basic delete
+    await connections.deleteFolder('public_folder_1');
+    expect(allFolders.length).toEqual(3);
+
+    //basic delete 2
+    await connections.deleteFolder('private_folder_1');
+    expect(allFolders.length).toEqual(2);
+
+    //delete previously deleted folder
+    await connections.deleteFolder('private_folder_1'); 
+    expect(allFolders.length).toEqual(2);
+
+    //delete non exitent folder
+    await connections.deleteFolder('non existent folder');
+    expect(allFolders.length).toEqual(2);
+});
+
+test('test deleteNote', () => {
+    //this might not be implemented...
+});
