9f353c76911533ecaed46e0004d5b949eb0dcead
matthewzelten <57920495+matthewzelten@users.noreply.github.com>
2021-12-01 10:59:52 -0800

    update more

--- backend/sample.js
+++ backend/sample.js
@@ -7,22 +7,21 @@ const {
     //addFolder,
     addNote,
     deleteFolder,
-    deleteNote,
-} = require("./connections");
+    deleteNote} = require('./connections');
 const app = express();
 const cors = require("cors");
 const port = 5000;
-const { json } = require("express");
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.json());
 // main page: get all folders
 app.get("/", async (req, res) => {
     const allFolders = await getAllFolders();
     res.send(allFolders);
 });
+
+// folder page: get all notes
 app.get("/:folderName", async (req, res) => {
     const folderName = req.params["folderName"];
     const passw = req.query["pass"];
@@ -42,134 +41,32 @@ app.get("/:folderName", async (req, res) => {
         }
     }
 });
-// handle open folder, add folder and search folder
-//I use post methods here because it needs to get password for private folders
-//and I can't think of an alternative.
-//Please comment if you have any suggestions or alternative solutions!!!
-app.post("/", async (req, res) => {
-    const folder = req.body["folderName"];
-    const keyword = req.body["keyword"];
-    if (
-        (folder === undefined || folder.length == 0) &&
-        (keyword === undefined || keyword.length == 0)
-    ) {
-        let isDup = await findFolder(req.body["name"]);
-        if (isDup === undefined || isDup.length == 0) {
-            try {
-                const folderToAdd = new Folder(req.body);
-                folderToAdd.save();
-                res.status(200).send(folderToAdd).end();
-            } catch (error) {
-                console.log(error);
-                res.status(404).end();
-            }
-        } else {
-            res.status(404)
-                .send("Duplicate file name. " + isDup)
-                .end();
-        }
-    } else if (keyword === undefined || keyword.length == 0) {
-        let result = await findFolder(folder);
+//open note
+app.post("/:folderName/:note", (req, res) => {
+    const fName = req.params["folderName"];
+    const noteToGet = req.params["note"];
+    const result = findFolder(fName);
+    if (result === undefined || result.length == 0) {
+        res.status(404).send("Folder not found.");
+    } else {
+        result = findNote(fName, noteToGet);
         if (result === undefined || result.length == 0) {
-            res.status(404).send("Folder not found.");
+            res.status(404).send("Note not found.");
         } else {
             if (!result["isPrivate"]) {
-                res.status(200).send(result);
+                res.status(201).send(result).end();
             } else {
-                let passw = req.body["password"];
+                const passw = req.body;
                 if (passw["password"] === result["password"]) {
-                    //res.status(200).send(result);
-                    res.status(200).send("get notes");
-                    //res.status(200).send(findNotesByFolder(folderName));
+                    res.status(201).send(result).end();
                 } else {
-                    //res.status(404).send(req.body);
                     res.status(404).send("Wrong password. Access denied.");
                 }
             }
         }
-    } else {
-        //search folder
-        Folder.find({ name: { $regex: keyword, $options: "i" } }).exec(
-            function (err, docs) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.status(200).send(docs).end();
-                }
-            }
-        );
-        //res.status(200).send(result).end();
     }
 });
 
-//handle open note and add note
-//I use post methods here because it needs to get password for private notes
-//and I can't think of an alternative.
-//Please comment if you have any suggestions or alternative solutions!!!
-app.post("/:folderName", async (req, res) => {
-    const fName = req.params["folderName"];
-    const note = req.body["noteName"];
-    const keyword = req.body["keyword"];
-    if (
-        (note === undefined || note.length == 0) &&
-        (keyword === undefined || keyword.length == 0)
-    ) {
-        //add note
-        const noteToAdd = req.body;
-        let result = await findNote(fName, req.body["name"]);
-        if (result === undefined || result.length == 0) {
-            if (addNote(noteToAdd)) {
-                res.status(201).send(noteToAdd).end();
-            } else {
-                res.status(404).end();
-            }
-        } else {
-            res.status(404).send("Duplicate file name.").end();
-        }
-    } else if (keyword === undefined || keyword.length == 0) {
-        result = await findFolder(fName);
-        if (result === undefined || result.length == 0) {
-            res.status(404).send("Folder not found.");
-        } else {
-            result = await findNote(fName, noteToGet);
-            if (result === undefined || result.length == 0) {
-                res.status(404).send("Note not found.");
-            } else {
-                if (!result["isPrivate"]) {
-                    res.status(200).send(result).end();
-                } else {
-                    const passw = req.body;
-                    if (passw["password"] === result["password"]) {
-                        res.status(200).send(result).end();
-                    } else {
-                        res.status(404).send("Wrong password. Access denied.");
-                    }
-                }
-            }
-        }
-    } else {
-        //search note
-        Note.find({ name: { $regex: keyword, $options: "i" } }).exec(function (
-            err,
-            docs
-        ) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.status(200).send(docs).end();
-            }
-        });
-    }
-});
-
-/* merged into one post function. but I keep this for now just in case.
-app.post("/:folderName", async (req, res) => {
-  const noteToAdd = req.body;
-  const fName = req.params["folderName"];
-  let result = await findNote(fName, req.body["name"]);
-  if(result===undefined||result.length==0){
-    if(addNote(noteToAdd)){
-      res.status(201).send(noteToAdd).end();
 //search note
 app.get("/:folderName", (req, res) => {});
 //add folder
@@ -184,32 +81,40 @@ app.post("/", (req, res) => {
     } else {
         res.status(404).send("Duplicate file name.").end();
     }
-    else{
-      res.status(404).end();
+});
+//add note
+app.post("/:folderName/:noteName", (req, res) => {
+    const noteToAdd = req.params["noteName"];
+    const fName = req.params["folderName"];
+    let result = findNote(fName, noteToAdd["name"]);
+    if (result === undefined || result.length == 0) {
+        noteToAdd.noteContent = [{}];
+        addNote(fName, noteToAdd);
+        res.status(201).send(noteToAdd).end();
+    } else {
+        res.status(404).send("Duplicate note name.").end();
     }
-  }
-  else{
-    res.status(404).send("Duplicate file name.").end();
-  }
-});*/
+});
 //delete folder
 app.delete("/", (req, res) => {
     const folderToDelete = req.body["name"];
-    if (deleteFolder(folderToDelete)) {
+    let result = findFolder(folderToDelete);
+    if (result === undefined || result.length == 0) {
+        res.status(404).send(folderToDelete);
+    } else {
         deleteFolder(folderToDelete);
         res.status(204).end();
-    } else {
-        res.status(404).send(folderToDelete);
     }
 });
 //delete note
 app.delete("/:folderName", (req, res) => {
     const noteToDelete = req.body["name"];
-    const folderN = req.params["folderName"];
-    if (deleteNote(folderN, noteToDelete)) {
-        res.status(204).end();
+    let result = findNote(folderName, noteToDelete);
+    if (result === undefined || result.length == 0) {
+        res.status(404).send(noteToDelete);
     } else {
-        res.status(404).end();
+        deleteNote(folderName, noteToDelete);
+        res.status(204).end();
     }
 });
 app.listen(port, () => {
